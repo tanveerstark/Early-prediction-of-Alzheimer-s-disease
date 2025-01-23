57f7fa1a5d770eec1d302fef0faee66872613772
tanveerstark <tanutanveer246@gmail.com>
2025-01-23 23:32:35 +0530

    Update and rename main.py to  training the model.py

---
+++ training the model.py
@@ -0,0 +1,8 @@
+# Train the model
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // train_generator.batch_size,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // validation_generator.batch_size,
+    epochs=20
+)
